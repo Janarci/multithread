6a11dfd5f7ac85e39b79137690ba78107348052d
Janarci <86641308+Janarci@users.noreply.github.com>
2023-03-11 06:32:09 +0800

    wee

--- IETThread.h
+++ IETThread.h
@@ -2,10 +2,7 @@
 #include <iostream>
 #include <thread>
 
-/// <summary>
-/// Representation of a Java/C# like thread class. The behavior must be inherited by a class that needs to be threaded.
-/// By: NeilDG
-/// </summary>
+
 class IETThread
 {
 public:
